1ea7db83d60751f14d45660e7948796d24c3d383
GabrielaDiazB <gabriela.dbravo@gmail.com>
2019-05-14 17:32:57 -0500

    Se crearon los componentes donde entrarán los temas de cada curso y sus contenidos

--- impulse-app/src/app/app.module.ts
+++ impulse-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { TopicsComponent } from './components/courses/topics/topics.component';
+import { TopicmultimediaComponent } from './components/courses/topicmultimedia/topicmultimedia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    TopicsComponent,
+    TopicmultimediaComponent
   ],
   imports: [
     BrowserModule,
---
+++ impulse-app/src/app/components/courses/topicmultimedia/topicmultimedia.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-topicmultimedia',
+  templateUrl: './topicmultimedia.component.html',
+  styleUrls: ['./topicmultimedia.component.css']
+})
+export class TopicmultimediaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
