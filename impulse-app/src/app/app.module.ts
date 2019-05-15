26ea65eb6f0ed91308d12b7aebb9f882efb8fe9c
GabrielaDiazB <gabriela.dbravo@gmail.com>
2019-05-15 15:56:33 -0500

    se corrigieron bugs, se añadio link del marvel, se agrego estilo al footer

--- impulse-app/src/app/app.module.ts
+++ impulse-app/src/app/app.module.ts
@@ -24,6 +24,7 @@ import { SkillLevelComponent } from './components/quiz/skill-level/skill-level.c
 import { RealcaseTestComponent } from './components/realcase-test/realcase-test.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
@@ -41,7 +42,8 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
     SkillLevelComponent,
     RealcaseTestComponent,
     HomeComponent,
-    NavBarComponent
+    NavBarComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
